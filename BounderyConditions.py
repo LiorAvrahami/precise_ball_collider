154f82eb98b3c42930254f2d42cec25c0d53be3e
LiorAvrahami <57775835+LiorAvrahami@users.noreply.github.com>
2019-11-15 14:27:44 +0200

    small fix to BounderyConditions

--- BounderyConditions.py
+++ BounderyConditions.py
@@ -7,19 +7,19 @@ class BounderyConditions(abc.ABC):
     @abc.abstractmethod
     def calc_collision_time_with_walls(self, ball: Ball) -> Tuple[float, int]:
         """
-        :return: a tuple: (collision time, index of colliding wall) the tuple's contents should be passed in this order to "handle_wall_collision"
+        :return: a tuple: (collision time, index of colliding wall) the "index of colliding wall" should be passed to "handle_wall_collision"
         """
         pass
 
     @abc.abstractmethod
     def handle_wall_collision(self,ball: Ball,wall_index: int):
         """
-        :param wall_index: the wall_index of colliding wall, this value is returned automatically from "calc_collision_time_with_walls"
+        :param wall_index: the wall_index of colliding wall, this value is returned from "calc_collision_time_with_walls"
         """
         pass
 
     @staticmethod
-    def calc_collision_time_with_wall_1d(ball_x,wall_x,ball_vel_x):
+    def _calc_collision_time_with_wall_1d(ball_x,wall_x,ball_vel_x):
         ret = (wall_x-ball_x)/ball_vel_x
         return ret if ret > 0 else np.inf
 
@@ -44,10 +44,10 @@ class RectangleBoundery_2D(BounderyConditions):
         """
         :return: a tuple: (collision time, index of colliding wall) the tuple's contents should be passed in this order to "handle_wall_collision"
         """
-        ret_1 = self.calc_collision_time_with_wall_1d(ball.location[0],self.wall_x_0,ball.velocity[0]) , self.wall_x_0_index
-        ret_2 = self.calc_collision_time_with_wall_1d(ball.location[0],self.wall_x_1,ball.velocity[0]) , self.wall_x_1_index
-        ret_3 = self.calc_collision_time_with_wall_1d(ball.location[1],self.wall_y_0,ball.velocity[1]) , self.wall_y_0_index
-        ret_4 = self.calc_collision_time_with_wall_1d(ball.location[1],self.wall_y_1,ball.velocity[1]) , self.wall_y_1_index
+        ret_1 = self._calc_collision_time_with_wall_1d(ball.location[0],self.wall_x_0,ball.velocity[0]) , self.wall_x_0_index
+        ret_2 = self._calc_collision_time_with_wall_1d(ball.location[0],self.wall_x_1,ball.velocity[0]) , self.wall_x_1_index
+        ret_3 = self._calc_collision_time_with_wall_1d(ball.location[1],self.wall_y_0,ball.velocity[1]) , self.wall_y_0_index
+        ret_4 = self._calc_collision_time_with_wall_1d(ball.location[1],self.wall_y_1,ball.velocity[1]) , self.wall_y_1_index
         return min(ret_1,ret_2,ret_3,ret_4)
 
 class CyclicBounderyConditions_2D(RectangleBoundery_2D):
